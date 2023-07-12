e61d3ad622fd9d9ed3daf70f5e60fcf707f03c44
Faidra Antoniadou <63123583+faantoniadou@users.noreply.github.com>
2023-07-12 18:09:35 +0100

    trying to get the grasp of this

--- src/main.js
+++ src/main.js
@@ -1,6 +1,22 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './assets/tailwind.css'
+import PrimeVue from 'primevue/config';
+import './styles/custom.css';
+import { router } from "./router";
+
+import "primeflex/primeflex.css";
+import "primevue/resources/themes/lara-light-blue/theme.css";
+import "primevue/resources/primevue.min.css"; /* Deprecated */
+import "primeicons/primeicons.css";
+
+const app = createApp(App);
+
+import Dropdown from 'primevue/dropdown';
+
+app.use(router);
+app.component('Dropdown', Dropdown);
 
 
 createApp(App).mount('#app')
+app.use(PrimeVue);
