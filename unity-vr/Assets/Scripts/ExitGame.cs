8301e4f075e4bb8cdc623113e00d7f117847877c
Faidra Antoniadou <63123583+faantoniadou@users.noreply.github.com>
2023-08-09 23:43:35 +0100

    this works

--- unity-vr/Assets/Scripts/ExitGame.cs
+++ unity-vr/Assets/Scripts/ExitGame.cs
@@ -1,19 +1,13 @@
 using UnityEngine;
-using System.Runtime.InteropServices;
-
 
 public class ExitGame : MonoBehaviour
 {
-    [DllImport("__Internal")]
-    private static extern void closewindow();
-
     public void QuitGame()
     {
 #if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
 #else
         Application.Quit();
-        closewindow();  
 #endif
     }
 }
