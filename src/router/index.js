2746f2e10690acd698b610e1fb17593cca646747
Faidra Antoniadou <63123583+faantoniadou@users.noreply.github.com>
2023-08-11 13:31:56 +0100

    adding qiskit schematics table

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../views/Home.vue';
 import Learning from '../views/Learning.vue';
+import QiskitSchematicsTable from '../views/QiskitSchematicsTable.vue';
 
 
 const routes = [
   {path: '/', name: 'Home', component: Home},
   {path: '/learning', name: 'Learning', component: Learning},
+  {path: '/qiskit-schematics-table', name: 'QiskitSchematicsTable', component: QiskitSchematicsTable},
 ];
 
 export default routes;
