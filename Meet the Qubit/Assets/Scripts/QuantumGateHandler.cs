f388151edfe16b6287e9666d6c8210880a0c16c2
Faidra Antoniadou <63123583+faantoniadou@users.noreply.github.com>
2023-08-25 13:39:08 +0100

    trying to set up flask api

---
+++ Meet the Qubit/Assets/Scripts/QuantumGateHandler.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class QuantumGateHandler : MonoBehaviour
+{
+    public Transform quboTransform;
+    public float blochSphereRadius = 12.95124f;  // The radius of the Bloch sphere
+
+    private string baseURL = "http://127.0.0.1:5000";
+
+    [System.Serializable]
+    public class BlochVectorResponse
+    {
+        public float[] bloch_vector;
+    }
+
+    public void OnHadamardGateClick()
+    {
+        StartCoroutine(ApplyGate("hadamard"));
+    }
+
+    private IEnumerator ApplyGate(string gateName)
+    {
+        string url = baseURL + "/apply_gate";
+
+        UnityWebRequest www = new UnityWebRequest(url, "POST");
+        byte[] bodyRaw = System.Text.Encoding.UTF8.GetBytes("{\"gate_name\": \"" + gateName + "\"}");
+        www.uploadHandler = (UploadHandler)new UploadHandlerRaw(bodyRaw);
+        www.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+        www.SetRequestHeader("Content-Type", "application/json");
+
+        yield return www.SendWebRequest();
+
+        if (www.result != UnityWebRequest.Result.Success)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            string jsonResponse = www.downloadHandler.text;
+
+            BlochVectorResponse response = JsonUtility.FromJson<BlochVectorResponse>(jsonResponse);
+            Vector3 blochVector = new Vector3(response.bloch_vector[0], response.bloch_vector[1], response.bloch_vector[2]);
+
+            MoveQuboToPosition(blochVector);
+        }
+    }
+
+    private void MoveQuboToPosition(Vector3 blochVector)
+    {
+        blochVector.Normalize();
+        Vector3 targetPosition = blochVector * blochSphereRadius;
+        quboTransform.position = targetPosition;
+    }
+}
--- qiskit_backend/server.py
+++ qiskit_backend/server.py
@@ -1,20 +1,32 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import qiskit
+from qiskit.visualization import plot_bloch_multivector
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/apply_gate', methods=['POST'])
+
 def apply_gate():
     data = request.json
-    gate_name = data['gate']
-    qubo_state = data['qubo_state']
+    gate_name = data['gate_name']  # Adjusted to match Unity's POST data
     
     # Use Qiskit here to apply the gate on Qubo's state, then calculate its position on the Bloch sphere.
+    qc = qiskit.QuantumCircuit(1)
     
-    # Placeholder return (have to replace this with actual calculations)
-    return jsonify({'new_position': [0, 0, 1]})
+    if gate_name == 'hadamard':
+        qc.h(0)
+
+    backend = qiskit.Aer.get_backend('statevector_simulator')
+    result = qiskit.execute(qc, backend).result()
+    state = result.get_statevector(qc)
+    
+    # Get the Bloch sphere coordinates
+    bloch = qiskit.visualization.plot_bloch_multivector(state, title="New State of Qubo", return_fig_data=True)
+    
+    # Return the bloch vector
+    return jsonify({"bloch_vector": list(bloch)})
 
 if __name__ == '__main__':
     app.run(debug=True)
