224c58836f3cd9d9030947bb6dbf591e7e2540dd
Faidra Antoniadou <63123583+faantoniadou@users.noreply.github.com>
2023-08-31 16:44:48 +0100

    small changes

--- src/server.js
+++ src/server.js
@@ -1,27 +1,32 @@
 import express from 'express';
+import path from 'path';
+
 let server;
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
 function startServer(courseTitle, callback) {
   // Course URLs for different courses'
   const courseURLs = {
-    "The Quantum Computer": "/path/to/quantum-computer-course",
-    "QiSkit Schematics": "/path/to/qiskit-schematics-course",
-    "The Bloch Sphere": "/path/to/qiskit-schematics-course"
+    "The Quantum Computer": "/unity-vr",
+    "The Bloch Sphere": "The Bloch Sphere"
     // ... other courses
   };
 
   const courseURL = courseURLs[courseTitle];
 
   if (!courseURL) {
-    console.error('Course not found for course:', courseTitle);
+    console.error('Course not found:', courseTitle);
     return;
   }
 
   app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, courseURL));
+    res.sendFile(path.join(__dirname, '..', 'public', courseURL, 'index.html'));
   });
 
-  const server = app.listen(0, 'localhost', () => {
+  server = app.listen(0, 'localhost', () => {
     const port = server.address().port;
     console.log(`Course server listening at http://localhost:${port}`);
     callback(port);
@@ -30,7 +35,7 @@ function startServer(courseTitle, callback) {
   return server;
 }
 
-const stopServer = (callback) => {
+const stopServer = () => {
   if (server) {
     server.close(() => {
       console.log('Server stopped');
