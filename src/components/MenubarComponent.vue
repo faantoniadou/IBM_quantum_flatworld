0df14669fe5705ec8796318a5fc20387cf8125d1
Faidra Antoniadou <63123583+faantoniadou@users.noreply.github.com>
2023-09-11 17:32:40 +0100

    small adjustments

--- src/components/MenubarComponent.vue
+++ src/components/MenubarComponent.vue
@@ -1,7 +1,7 @@
 <template>
 		<Menubar style="padding: 20px;">
 			<template #start>
-					<img alt="logo" src="../assets/logo.png" height="20" width="100" class="mr-2" />
+					<img alt="logo" src="../assets/logo.png" height="20" width="170" class="mr-2" />
 			</template>
 			<template #end>
 			</template>
